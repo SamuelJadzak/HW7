09df197f61ff568b56cf8e249b3c5aae29c005c9
SamuelJadzak <43551888+SamuelJadzak@users.noreply.github.com>
2021-05-31 21:59:41 -0700

    Added test_multiples5
    implemented test "test_multiples5", which asserts equal the number of multiples of 5 in 100 (20) with the output of function "BuzzFunc."

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -6,5 +6,9 @@ class testcase2(unittest.TestCase):
         result = FizzBuzz.FizzFunc()
         self.assertEqual(result, 33)
 
+    def test_multiples5(self):
+        result = FizzBuzz.BuzzFunc()
+        self.assertEqual(result, 20)
+        
 if __name__ == '__main__':
     unittest.main()
