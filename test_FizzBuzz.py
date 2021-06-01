ccd0bf55d7f673099157c6caa82180790677fcc5
SamuelJadzak <43551888+SamuelJadzak@users.noreply.github.com>
2021-05-31 21:40:14 -0700

    Create test_FizzBuzz.py
    Describes test for a function FizzFunc

---
+++ test_FizzBuzz.py
@@ -0,0 +1,9 @@
+import unittest
+
+class testcase2(unittest.TestCase):
+    def test_multiples3(self):
+        result = FizzBuzz.FizzFunc()
+        self.assertEqual(result, 33)
+
+if __name__ == '__main__':
+    unittest.main()
