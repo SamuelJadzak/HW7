06d16d619235a8fc50daf3f7cc4672484dfe7cc6
SamuelJadzak <43551888+SamuelJadzak@users.noreply.github.com>
2021-05-31 23:52:54 -0700

    Added new test
    Added test to check whether a year IS a leap year. Expected output is true.

--- LeapYearFileTestDriven.py
+++ LeapYearFileTestDriven.py
@@ -3,4 +3,3 @@ def LeapYear(yearIn):
     if(year%100==0 and year%400!=0):
         print(str(year) +" is not a leap year")
         return False
-print(LeapYear(1700))
--- test_LeapYearFileTestDriven.py
+++ test_LeapYearFileTestDriven.py
@@ -6,7 +6,9 @@ class testcase2(unittest.TestCase):
         result = LeapYearFileTestDriven.LeapYear(1700)
         self.assertEqual(result, False)
 
-
+    def test_IsLeapYear(self):
+        result = LeapYearFileTestDriven.LeapYear(2000)
+        self.assertEqual(result, True)
 
 if __name__ == '__main__':
     unittest.main()
