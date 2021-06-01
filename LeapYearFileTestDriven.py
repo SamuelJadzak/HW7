0ecd1bdd1e0687c284eedda3439910be21b69143
SamuelJadzak <43551888+SamuelJadzak@users.noreply.github.com>
2021-05-31 23:53:54 -0700

    Added code to successfully pass unit test 2

--- LeapYearFileTestDriven.py
+++ LeapYearFileTestDriven.py
@@ -3,3 +3,9 @@ def LeapYear(yearIn):
     if(year%100==0 and year%400!=0):
         print(str(year) +" is not a leap year")
         return False
+    elif(year%4==0): 
+        print(str(year) +" is a leap year")
+        return True
+    else:
+        print(str(year) +" is not a leap year")
+        return False
