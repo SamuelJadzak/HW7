f92c08cef8ef0f9399500bc85a4078bfc8671056
SamuelJadzak <43551888+SamuelJadzak@users.noreply.github.com>
2021-05-31 22:02:58 -0700

    Implemented "BuzzFunc"
    Implemented function to correctly return the value of multiples of 5 in 100, passing the test test_multiples5

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -6,3 +6,10 @@ def FizzFunc():
     print("Fizz"*Fizz)
     return Fizz
 
+def BuzzFunc():
+    Buzz = 0
+    for x in range(100):
+        if(x % 5 == 0):
+            Buzz = Buzz + 1
+    print("Buzz"*Buzz)
+    return Buzz
